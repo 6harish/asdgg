745d392165b35cad083d8d6959d0abef751f1d48
harish seervi <49792825+6harish@users.noreply.github.com>
2023-03-06 22:53:56 +0530

    first commit

---
+++ src/bottom/Screen2.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Screen2 = () => {
+  return (
+    <View style={{backgroundColor: '#034', flex: 1}}>
+      <Text>Screen2</Text>
+    </View>
+  );
+};
+
+export default Screen2;
+
+const styles = StyleSheet.create({});
---
+++ src/bottom/Screen3.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Screen3 = () => {
+  return (
+    <View style={{backgroundColor: '#009', flex: 1}}>
+      <Text>Screen3</Text>
+    </View>
+  );
+};
+
+export default Screen3;
+
+const styles = StyleSheet.create({});
